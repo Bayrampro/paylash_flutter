22cc164e38a3c015e5e12c1da624fa94c448c25e
Bayrampro <annakurbanov02@gmail.com>
2024-11-02 23:08:16 +0300

    2 todo done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-//TODO: 1. Настроить зависимости wifi_iot и flutter_riverpod
-//TODO 2. Настроить разрешения в AndroidManifest.xml
 //TODO 3. Реализовать управление состоянием Hotspot с помощью Riverpod
 //TODO 5. Создать UI для проверки работы Hotspot
 
